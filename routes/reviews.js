95141896942e35bfb258f7818940ae2206240c62
Jasmine Hui <jasminehui293@gmail.com>
2022-12-01 11:27:08 +0000

    main file and routes

---
+++ routes/reviews.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router({ mergeParams: true });
+const Art = require('../models/art');
+const Review = require('../models/review');
+const { isLoggedIn, isAuthor, validateArt, isReviewAuthor, validateReview } = require('../middleware');
+const { reviewSchema } = require('../schemas.js');
+const catchAsync = require('../utils/catchAsync');
+const ExpressError = require('../utils/ExpressError');
+const reviews = require('../controllers/review')
+
+router.post('/', isLoggedIn, validateReview, catchAsync(reviews.createReview))
+
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(reviews.deleteReview))
+
+module.exports = router;
+
